7b979cb01678e291be8f1ec8bd2524c46ba40f83
jllewis11 <jllewis11@gmail.com>
2022-08-12 10:35:14 -0700

    Updated 3D model

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,9 +16,7 @@ const Home = () => {
             Software Engineer
           </h4>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            
           </p>
 
           <div>
