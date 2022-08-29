1dac5a98d483522694ab1fcd1cfef40ff37ecfc9
jllewis11 <jllewis11@gmail.com>
2022-08-28 23:28:00 -0700

    Updated web layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 //import About from './components/About'
+import React, { useEffect } from 'react'
 import Experience from './components/Experience'
 import Home from './components/Home'
 import NavBar from './components/NavBar'
@@ -6,14 +7,14 @@ import NavBar from './components/NavBar'
 import SocialLinks from './components/SocialLinks'
 
 function App() {
+  useEffect(() => {
+    document.title = "Jerry's Portfolio"
+  }, [])
   return (
     <div>
       <NavBar />
-
       <Home />
-
       <Experience />
-
       <SocialLinks />
     </div>
   )
