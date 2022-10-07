463e12aef7cc682fb33a1312a0b87d68d65fc781
jllewis11 <jllewis11@gmail.com>
2022-10-06 18:36:35 -0700

    Updated work exp map

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,42 +6,44 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+//Reference : https://mui.com/material-ui/react-card/
+//https://mui.com/material-ui/react-typography/
+
+
 const WorkExperience = () => {
     const workexp = [
       {
         id: 1,
-        title: 'Code Coach',
+        title: 'Code Coach - Part-Time',
         company: 'theCoderSchool - Cerritos, CA',
         date: 'September 2019 - March 2022',
+        description: "- Worked in 2-On-1's and large class settings teaching students from K-12 programming and creating projects.\n" +
+        "- Maintained a flexible timeline of progress as progression is different per student.\n" +
+        "- Building customer loyalty through excellent customer service and lesson quality, resulting in multiple 5-star reviews.\n" +
+        "- Assisted in language translations for customers that spoke Mandarin Chinese.\n" +
+        "- Fostered a teaching environment that successfully helped students get first place in theCoderSchool nationwide competition\n"
       },
       {
         id: 2,
-        src: csuf,
-        link: 'https://github.com/jllewis11/titan-talks-2.0'
+        title: 'Undergraduate Student Researcher',
+        company: 'California State University, Fullerton - Fullerton, CA',
+        date: 'March 2022 - June 2022' ,
+        description: "- Analyzed research around cortisol Activity in Individuals within an Educational Institution.\n" +
+        "- Extrapolated datasets to classify and test cortisol levels using Python Pandas and NumPy\n"
       },
       {
         id: 3,
-        src: lessons,
-        link: 'https://github.com/jllewis11/Python-Project-Lessons'
-      },
-      {
-        id: 4,
-        src: workle,
-        link: 'https://github.com/jllewis11/WorkleV3'
-      },
-      {
-        id: 5,
-        src: dsgo,
-        link: 'https://github.com/jllewis11/Data-Structures-in-Go'
-      },
-      {
-        id: 6,
-        src: portfolio,
-        link: 'https://github.com/jllewis11/jllewis11.github.io'
+        title: 'Software Engineer and Research Intern',
+        company: 'University of Southern California - Institute for Creative Technologies - Playa Vista, CA',
+        date: 'June 2022 - August 2022',
+        description: "- Expanded website capabilities of existing content registration portal to allow for web-authored content using React.js and Node.js.\n" +
+        "- Implemented automated tests to ensure reliability and code quality with Cypress Testing Framework.\n" +
+        "- Developed a Python script algorithm that determined permutations of sentences, increasing efficiency greatly. \n" +
+        "- Analyzed video and audio data for a machine learning task evaluating a US Army fireteam’s performance.\n"
       }
     ]
 
-
+  };
 const bull = (
   <Box
     component="span"
